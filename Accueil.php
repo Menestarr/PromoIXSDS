b5f6d13cc97523e144335b84bc8ded0a5018767b
XRay2404 <gregoireenzo@gmail.com>
2018-11-11 13:00:59 +0100

    Rename PhiRoyal -> Accueil

--- Accueil.php
+++ Accueil.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="PhiRoyal.css">
-	<title>PhiRoyal</title>
+	<title>Accueil</title>
 </head>
 
 <body>
-	
+	<h1>PhiRoyal</h1>
 </body>
 </html>
